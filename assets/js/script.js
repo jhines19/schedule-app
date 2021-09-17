98da9e0b0cf44ce846021238d7db7d6ad4e67e2c
Jaraad Hines <jaraadhines@Jaraads-MacBook-Pro-2.local>
2021-09-17 18:15:30 -0400

    set up header with real-time update and put first structure for schedule entries

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+//moment.js in header to get todays date displayed
+var todaysDate = moment();
+$("#today").text(todaysDate.format("dddd, MMMM Do YYYY"));
+
+//Activity 04 in Unit 06: example of dynamically creating list without having to type a bunch of times in html and 03 is example of table
+//also Activity 09
+
+
+//Getting some info rendered onto a seciton of the screen, Activity 10
+//could style via jquery as well if wanted to
+
+
+//click and type into cell
+//.on , event delegation, etc.
+
+
+//save your entry :activity 06 in unit 05
+//(practice saving something in localStorage. Havent even done that yet.)
